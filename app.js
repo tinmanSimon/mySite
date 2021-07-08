af4844f3f5e10127e46005f3b15a1da556367736
tinmanSimon <wudisas@gmail.com>
2021-07-07 23:04:45 -0400

    added MongoDB for main, about, contact and post

--- app.js
+++ app.js
@@ -8,34 +8,159 @@ const homeStartingContent = "Hi welcome to Simon's playground!";
 const aboutContent = "My name is Simon, you can call me tinman too. I'm a software engineer with an enthusiasm for graphics engineering and also web development";
 const contactContent = "you can contact me through my email at: simon@tinmansimon.com";
 const app = express();
-
 const _ = require('lodash');
-
-var posts = [];
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(rec, res){
-  res.render("home", {
-    homeStartingContent : homeStartingContent,
-    posts : posts
+const mongoose = require("mongoose");
+const mongoUrl = "";
+
+let debugMode = true;
+let metaData = {};
+let allPosts = [];
+function debugLog(s){
+  if (debugMode) {
+    console.log(s);
+  }
+}
+
+function errorLog(s){
+  console.log(s);
+}
+
+const postSchema = new mongoose.Schema({
+  title : {
+    type: String,
+    required: [true, "no title"]
+  },
+  hrefLink : {
+    type: String,
+    required: [true, "no hrefLink"]
+  },
+  content : {
+    type: String,
+    required: [true, "no content"]
+  },
+  truncatedContent : {
+    type: String,
+    required: [true, "no truncatedContent"]
+  }
+});
+const PostModel = mongoose.model("post", postSchema, "posts");
+const metaDataSchema =  new mongoose.Schema({
+  homeStartingContent : String,
+  aboutContent : String,
+  contactContent : String
+});
+const MetaModel = mongoose.model("meta", metaDataSchema, "metadata");
+
+function connectMongoose(url){
+  debugLog("connect using mongoose");
+  mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+  findAllAndCallBack.connectedToMongoose = true;
+}
+
+function findAllAndCallBack(url, params){
+  if (findAllAndCallBack.connectedToMongoose === undefined) {
+    findAllAndCallBack.connectedToMongoose = false;
+  }
+  if (!findAllAndCallBack.connectedToMongoose) connectMongoose(url);
+
+  if (Array.isArray(params.callbackParamsList) && params.callbackParamsList.length > 0) {
+    param = params.callbackParamsList.pop();
+    if ((!param.hasOwnProperty("callback")) || (!param.hasOwnProperty("model"))) return;
+  }
+  else return;
+
+  param.model.find({}, function(err, docs){
+    if (err) {
+      errorLog(err);
+    } else {
+      debugLog("succeed!");
+      param.callback(docs, params);
+    }
   });
-})
+}
+
+function homePagePostsCallback(posts, params) {
+  if (!params.res) { return; }
+  allPosts = posts;
+  params.res.render("home", {
+    homeStartingContent : metaData.homeStartingContent,
+    posts : allPosts
+  });
+}
+
+
+function homepageMetaCallback(metas, params){
+  if (metas.length != 1) {
+    errorLog("metalCallback have 0 or more than 1 metadata records in database!");
+    callbackParams.res.render("home", {
+      homeStartingContent : "Error homepage can't get meta data!",
+      posts : []
+    });
+    return;
+  }
+  metaData = metas[0];
+  if (params.renderOnMetaCallback === true) {
+    params.res.render(params.renderTitle, metaData);
+  }
+  findAllAndCallBack(mongoUrl, params);
+}
+
+function getMetaAndRender(res, renderTitle, directRenderObj, renderOnMetaCallback = false){
+  //if metaData is incomplete, get data first.
+  if ((allPosts.length === 0) || (metaData.aboutContent === undefined)) {
+    debugLog("allPosts.length = " + allPosts.length);
+    debugLog("metaData: ");
+    debugLog(metaData);
+    findAllAndCallBack(mongoUrl, {
+      res : res,
+      callbackParamsList : [{
+        callback : homepageMetaCallback,
+        model : MetaModel
+      }],
+      renderOnMetaCallback : renderOnMetaCallback,
+      renderTitle : renderTitle
+    });
+  } else {
+    res.render(renderTitle, directRenderObj);
+  }
+}
+
+
+app.get("/", function(rec, res){
+  if (allPosts.length === 0 || !metaData.hasOwnProperty("homeStartingContent")) {
+    findAllAndCallBack(mongoUrl, {
+      res : res,
+      callbackParamsList : [{
+          callback : homePagePostsCallback,
+          model : PostModel
+        }, {
+          callback : homepageMetaCallback,
+          model : MetaModel
+      }]
+    });
+  } else {
+    res.render("home", {
+      homeStartingContent : metaData.homeStartingContent,
+      posts : allPosts
+    });
+  }
+});
 
 app.get("/about", function(rec, res){
-  res.render("about", {aboutContent : aboutContent});
-})
+  getMetaAndRender(res, "about", {aboutContent : metaData.aboutContent}, true);
+});
 
 app.get("/contact", function(rec, res){
-  res.render("contact", {contactContent : contactContent});
-})
+  getMetaAndRender(res, "contact", {contactContent : metaData.contactContent}, true);
+});
 
 app.get("/compose", function(rec, res){
   res.render("compose");
-})
+});
 
 app.get("/posts/:post", function(rec, res){
   let post = rec.params.post;
@@ -55,17 +180,24 @@ app.get("/posts/:post", function(rec, res){
       content : ""
     });
   }
-})
+});
 
 app.post("/compose", function(rec, res){
-  posts.push({
+  postItem = {
     title : rec.body.composeTitle,
     hrefLink : rec.body.composeTitle.replace(/\s/g, "-"),
     content : rec.body.composeContent,
     truncatedContent : _.truncate(rec.body.composeContent, {'length' : 100})
+  };
+  mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+  const newItem = new PostModel(postItem);
+  newItem.save(function(err){
+    if (err) {
+      errorLog(err);
+    } else return;
   });
   res.redirect("/");
-})
+});
 
 app.listen(port, function() {
   console.log("Server started on port 3000");
